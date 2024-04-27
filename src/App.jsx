877fd6b91b8383a877baf84eb47732f6aa0a2070
Saikat Hazra <saikathazra9732@gmail.com>
2024-04-27 19:26:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hi
+    </>
+  )
+}
+
+export default App
---
+++ src/dataExamples.jsx
@@ -0,0 +1,41 @@
+let data = {
+    "count": 4,
+    "recipes": [
+      {
+        "publisher": "101 Cookbooks",
+        "title": "Best Pizza Dough Ever",
+        "source_url": "http://www.101cookbooks.com/archives/001199.html",
+        "recipe_id": "47746",
+        "image_url": "http://forkify-api.herokuapp.com/images/best_pizza_dough_recipe1b20.jpg",
+        "social_rank": 100,
+        "publisher_url": "http://www.101cookbooks.com"
+      },
+      {
+        "publisher": "The Pioneer Woman",
+        "title": "Deep Dish Fruit Pizza",
+        "source_url": "http://thepioneerwoman.com/cooking/2012/01/fruit-pizza/",
+        "recipe_id": "46956",
+        "image_url": "http://forkify-api.herokuapp.com/images/fruitpizza9a19.jpg",
+        "social_rank": 100,
+        "publisher_url": "http://thepioneerwoman.com"
+      },
+      {
+        "publisher": "Closet Cooking",
+        "title": "Pizza Dip",
+        "source_url": "http://www.closetcooking.com/2011/03/pizza-dip.html",
+        "recipe_id": "35477",
+        "image_url": "http://forkify-api.herokuapp.com/images/Pizza2BDip2B12B500c4c0a26c.jpg",
+        "social_rank": 99.9999999999999,
+        "publisher_url": "http://closetcooking.com"
+      },
+      {
+        "publisher": "Closet Cooking",
+        "title": "Cauliflower Pizza Crust (with BBQ Chicken Pizza)",
+        "source_url": "http://www.closetcooking.com/2013/02/cauliflower-pizza-crust-with-bbq.html",
+        "recipe_id": "41470",
+        "image_url": "http://forkify-api.herokuapp.com/images/BBQChickenPizzawithCauliflowerCrust5004699695624ce.jpg",
+        "social_rank": 99.9999999999994,
+        "publisher_url": "http://closetcooking.com"
+      },
+    ]
+}
